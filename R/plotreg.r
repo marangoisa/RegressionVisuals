2139c9d43dff995b6294afc35afbd6be35ff4539
marangoisa <31993368+marangoisa@users.noreply.github.com>
2019-07-22 19:38:00 -0500

    Update plotreg.r

--- R/plotreg.r
+++ R/plotreg.r
@@ -21,8 +21,8 @@
 #' @examples  
 #' exmp<-plotlreg(x='Distance_from_Largest_City_to_NYC_miles',y='Total_Trade_Amount_2015_Thousand_USD',titl='trade',xlab = 'distance',ylab='trade',w='GDP_2015_USD',names='Name_country',,nx=100,ny=0)
 #' exmp
-plotreg<-function(x,y,dat,titl,xlab,ylab,scx,scy,w,names,sepa,ini,font,logs,cv,nx,ny){
-  if(!require(ggplot2, quietly = TRUE)){install.packages('ggplot',dep = TRUE,quietly = TRUE)}
+plotreg<-function(x,y,dat,titl,xlab,ylab,scx,scy,w,names,sepa,ini,font,logs,cv,nx,ny,stepx,stepy){
+  if(require(ggplot2, quietly = TRUE)){install.packages('ggplot2',dep = TRUE,quietly = TRUE)}
   if(missing(titl)){titl<-' '}
   if(missing(xlab)){xlab<-'x'}
   if(missing(ylab)){ylab<-'y'}
@@ -32,38 +32,43 @@ plotreg<-function(x,y,dat,titl,xlab,ylab,scx,scy,w,names,sepa,ini,font,logs,cv,n
   if(missing(sepa)){sepa<-1}
   if(missing(scx)){scx<-1}
   if(missing(scy)){scy<-1}
-  if(missing(names)){
-    dat$names<-' '
-    names<-'names'}
+  if(missing(names)){dat$names<-' '
+  names<-'names'}
   if(missing(logs)){logs<-0}
   if(missing(nx)){nx<-0}
   if(missing(ny)){ny<-0}
-  if(missing(cv)){
-    if(logs==0){fm<-as.formula("y ~ x")}
-    if(logs==1){fm<-as.formula("log(y) ~ log(x)")}
-    if(logs==2){fm<-as.formula("log(y) ~ x")}
-    if(logs==3){fm<-as.formula("y ~ log(x)")}}else{
-      if(logs==0){fm<-as.formula(paste("y ~ x+", paste(cv, collapse= "+")))
-        fmcv<-as.formula(paste("y ~", paste(cv, collapse= "+")))}
-      if(logs==1){fm<-as.formula(paste("log(y) ~ log(x)+", paste(cv, collapse= "+")))
-        fmcv<-as.formula(paste("log(y) ~", paste(cv, collapse= "+")))}
-      if(logs==2){fm<-as.formula(paste("log(y) ~ x+", paste(cv, collapse= "+")))
-        fmcv<-as.formula(paste("log(y) ~", paste(cv, collapse= "+")))}
-      if(logs==3){fm<-as.formula(paste("y ~ log(x)+", paste(cv, collapse= "+")))
-        fmcv<-as.formula(paste("y ~", paste(cv, collapse= "+")))}}
   names(dat)[names(dat)==x]<-'x'
   names(dat)[names(dat)==y]<-'y'
   names(dat)[names(dat)==w]<-'w'
   x<-dat$x
   y<-dat$y
   w<-dat$w
+  if(missing(cv)){
+    if(logs==0){fm<-as.formula("y ~ x")}
+    if(logs==1){if(min(y,na.rm = TRUE)<0|min(x,na.rm = TRUE)<0){stop("y or x is <0, and using log")}
+      fm<-as.formula("log(y) ~ log(x)")}
+    if(logs==2){if(min(y,na.rm = TRUE)<0){stop("y is <0, and using log")}
+      fm<-as.formula("log(y) ~ x")}
+    if(logs==3){if(min(x,na.rm = TRUE)<0){stop("x is <0, and using log")}
+      fm<-as.formula("y ~ log(x)")}}else{
+        if(logs==0){fm<-as.formula(paste("y ~ x+", paste(cv, collapse= "+")))
+        fmcv<-as.formula(paste("y ~", paste(cv, collapse= "+")))}
+        if(logs==1){if(min(y,na.rm = TRUE)<0|min(x,na.rm = TRUE)<0){stop("y or x is <0, and using log")}
+          fm<-as.formula(paste("log(y) ~ log(x)+", paste(cv, collapse= "+")))
+          fmcv<-as.formula(paste("log(y) ~", paste(cv, collapse= "+")))}
+        if(logs==2){if(min(y,na.rm = TRUE)<0){stop("y is <0, and using log")}
+          fm<-as.formula(paste("log(y) ~ x+", paste(cv, collapse= "+")))
+          fmcv<-as.formula(paste("log(y) ~", paste(cv, collapse= "+")))}
+        if(logs==3){if(min(x,na.rm = TRUE)<0){stop("x is <0, and using log")}
+          fm<-as.formula(paste("y ~ log(x)+", paste(cv, collapse= "+")))
+          fmcv<-as.formula(paste("y ~", paste(cv, collapse= "+")))}}
   if(!missing(cv)){seqcv<-summary(lm(fmcv,weights=w,data=dat))}
   seq<-summary(lm(fm,weights=w,data=dat))
   yhat<-dat$y-seq$residuals
   if(seq$coefficients[2]<0){sign<-'-'}else{sign<-'+'}
   if(abs(round(seq$coefficients[2],1))<0.1){coef2<-'0.0'}else{coef2<-toString(abs(round(seq$coefficients[2],1)))}
   if(abs(round(seq$coefficients[1],1))<0.1){coef1<-'0.0'}else{coef1<-toString(abs(round(seq$coefficients[1],1)))}
-  eql<-paste0(ylab,'=',coef1,sign,coef2,'(',xlab,') R2=',round(seq$r.squared,2))
+  eql<-paste0(ylab,'=',coef1,sign,coef2,' (',xlab,')',' R2=',round(seq$r.squared,2))
   names<-as.character(dat[,names(dat)==names])
   xma<-max(x,na.rm=TRUE)
   yma<-max(y,na.rm=TRUE)
@@ -101,53 +106,55 @@ plotreg<-function(x,y,dat,titl,xlab,ylab,scx,scy,w,names,sepa,ini,font,logs,cv,n
           minx<-log(((min(x,na.rm=TRUE))))
           maxy<-(ceiling(yma/(scy*10))*10)
           miny<-(floor(min(y,na.rm=TRUE)/(scy*10))*10)}
-  stepx<-seq(from =minx, to = maxx, by =((maxx-minx)/5))
-  stepy<-seq(from =miny, to = maxy, by =((maxy-miny)/5))
-  stepx<-exp(stepx)
-  stepy<-exp(stepy)
+  if(missing(stepx)){
+    stepx<-seq(from =minx, to = maxx, by =((maxx-minx)/5))}
+  if(missing(stepy)){
+    stepy<-seq(from =miny, to = maxy, by =((maxy-miny)/5))}
+  stepx_ex<-round(exp(stepx),0)
+  stepy_ex<-round(exp(stepy),0)
   for (i in 1:length(stepx)){stepx[i]<-round(stepx[i],digits = 0)
   stepy[i]<-round(stepy[i],digits = 0)}
   dataf<-data.frame(cbind(x,y,w))
   if(missing(cv)){
     if(logs==0){
       plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
-      ggtitle(titl)+
-      xlab(xlab)+ 
-      ylab(ylab)+
-      geom_smooth(method='lm', se = FALSE,aes(weight=w,colour=eql))+
-      theme_classic(base_family = font)+
-      theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
-      scale_colour_manual(name=' ',values="grey50")+
-      geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
+        ggtitle(titl)+
+        xlab(xlab)+ 
+        ylab(ylab)+
+        geom_smooth(method='lm', se = FALSE,aes(weight=w,colour=eql))+
+        theme_classic(base_family = font)+
+        theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
+        scale_colour_manual(name=' ',values="grey50")+
+        geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
   }else if(logs==1){
-    plot <- ggplot(dataf, aes(x=log(x/scx),y=log(y/scy)))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+    plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
       ggtitle(titl)+
       xlab(xlab)+ 
       ylab(ylab)+
-      scale_y_continuous(trans = 'log2',breaks=stepy)+
-      scale_x_continuous(trans = 'log2',breaks=stepx)+
+      scale_y_continuous(trans = 'log2',breaks=stepy_ex)+
+      scale_x_continuous(trans = 'log2',breaks=stepx_ex)+
       geom_smooth(method='lm', se = FALSE,aes(weight=w,colour=eql))+
       theme_classic(base_family = font)+
       theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
       scale_colour_manual(name=' ',values="grey50")+
       geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
   }else if(logs==2){
-    plot <- ggplot(dataf, aes(x=x/scx,y=log(y/scy)))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+    plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
       ggtitle(titl)+
       xlab(xlab)+ 
       ylab(ylab)+
-      scale_y_continuous(trans = 'log2',breaks=stepy)+
+      scale_y_continuous(trans = 'log2',breaks=stepy_ex)+
       geom_smooth(method='lm', se = FALSE,aes(weight=w,colour=eql))+
       theme_classic(base_family = font)+
       theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
       scale_colour_manual(name=' ',values="grey50")+
       geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
   }else if(logs==3){
-    plot <- ggplot(dataf, aes(x=log(x/scx),y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+    plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
       ggtitle(titl)+
       xlab(xlab)+ 
       ylab(ylab)+
-      scale_x_continuous(trans = 'log2',breaks=stepx)+
+      scale_x_continuous(trans = 'log2',breaks=stepx_ex)+
       geom_smooth(method='lm', se = FALSE,aes(weight=w,colour=eql))+
       theme_classic(base_family = font)+
       theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
@@ -166,37 +173,37 @@ plotreg<-function(x,y,dat,titl,xlab,ylab,scx,scy,w,names,sepa,ini,font,logs,cv,n
         scale_colour_manual(name=' ',values="grey50")+
         geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
     }else if(logs==1){
-      plot <- ggplot(dataf, aes(x=log(x/scx),y=log(y/scy)))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+      plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
         geom_point(aes(y=yhat, size = w), color="grey50",show.legend = FALSE,alpha = 1/2)+
         ggtitle(titl)+
         xlab(xlab)+ 
         ylab(ylab)+
-        scale_y_continuous(trans = 'log2',breaks=stepy)+
-        scale_x_continuous(trans = 'log2',breaks=stepx)+
+        scale_y_continuous(trans = 'log2',breaks=stepy_ex)+
+        scale_x_continuous(trans = 'log2',breaks=stepx_ex)+
         geom_smooth(method='lm', se = FALSE,aes(y=yhat,weight=w,colour=eql))+
         theme_classic(base_family = font)+
         theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
         scale_colour_manual(name=' ',values="grey50")+
         geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
     }else if(logs==2){
-      plot <- ggplot(dataf, aes(x=x/scx,y=log(y/scy)))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+      plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
         geom_point(aes(y=yhat, size = w), color="grey50",show.legend = FALSE,alpha = 1/2)+
         ggtitle(titl)+
         xlab(xlab)+ 
         ylab(ylab)+
-        scale_y_continuous(trans = 'log2',breaks=stepy)+
+        scale_y_continuous(trans = 'log2',breaks=stepy_ex)+
         geom_smooth(method='lm', se = FALSE,aes(y=yhat,weight=w,colour=eql))+
         theme_classic(base_family = font)+
         theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
         scale_colour_manual(name=' ',values="grey50")+
         geom_text(aes(label=names_sh),size = 3,family = font,nudge_x=nx,nudge_y=ny)
     }else if(logs==3){
-      plot <- ggplot(dataf, aes(x=log(x/scx),y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
+      plot <- ggplot(dataf, aes(x=x/scx,y=y/scy))+geom_point(aes(size = w),show.legend = FALSE,alpha = 1/2)+
         geom_point(aes(y=yhat, size = w), color="grey50",show.legend = FALSE,alpha = 1/2)+
         ggtitle(titl)+
         xlab(xlab)+ 
         ylab(ylab)+
-        scale_x_continuous(trans = 'log2',breaks=stepx)+
+        scale_x_continuous(trans = 'log2',breaks=stepx_ex)+
         geom_smooth(method='lm', se = FALSE,aes(y=yhat,weight=w,colour=eql))+
         theme_classic(base_family = font)+
         theme(legend.position = "bottom",plot.title = element_text(hjust = 0.5))+
